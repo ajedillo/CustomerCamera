27c0769ee66aafdfb82456d3be75d94cbc25ddfe
Kip Tucker <katucke3@oakland.edu>
2018-10-11 10:54:03 -0400

    Corrected URLs

--- AccountSetup/djangotutorial/myproject/accounts/views.py
+++ AccountSetup/djangotutorial/myproject/accounts/views.py
@@ -3,8 +3,13 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 class SignUp(generic.CreateView):
+
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+class about(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('about')
+    template_name = 'about.html'
 # Create your views here.
